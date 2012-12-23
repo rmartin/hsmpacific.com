14696770da86172fc40657e845c6436093940aef
Roy Martin <roy.martin@modop.net>
2012-12-23 12:26:32 -0800

    Created application using Refinery CMS 2.0.9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HsmpacificCom::Application.config.secret_token = 'c10199a250a6b85ea08a770cb413c2813a56c321a835697025d3b5295792b24a28c81a7c7a827445a65062c09e38ab8dd6303c721ab2d148194e66358334cb6c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HsmpacificCom::Application.config.session_store :cookie_store, key: '_hsmpacific_com_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HsmpacificCom::Application.config.session_store :active_record_store
