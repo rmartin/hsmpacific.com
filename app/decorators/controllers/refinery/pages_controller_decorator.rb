eec050701eb556ee5029486fbf1a3a4443fa95e6
Roy Martin <roy.martin@modop.net>
2013-02-17 23:11:00 -0800

    Finished integration with search.

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -3,7 +3,7 @@ Refinery::PagesController.class_eval do
     before_filter :find_all_brokers, :only => [:show]
     before_filter :find_all_tenants, :only => [:show]
     before_filter :find_exclusive_tenants, :only => [:home]
-    before_filter :find_all_properties
+    before_filter :find_all_properties, :onluy => [:"Avaiable Properties"]
   
     protected
     
@@ -20,7 +20,39 @@ Refinery::PagesController.class_eval do
       end
       
       def find_all_properties
-        @properties = Refinery::Properties::Property.order("title ASC")
+        @cities = Refinery::Properties::City.order("name ASC")
+        @areaCodes = Refinery::Properties::AreaCode.order("name ASC")
+        @brokers = Refinery::Brokers::Broker.all
+        
+        #order params
+        @propertyTypes = ['Sale', 'Lease']
+        @stateCodes = ['Oregon', 'Washington']
+        @sortBy = ['Title', 'City', 'Area', 'State']
+        
+        
+        #filter parameters
+        @filterParams = params[:filter]
+        
+        @properties = Refinery::Properties::Property.order("title ASC");
+       
+        if @filterParams != nil
+          
+          if @filterParams['city_id'] != nil and @filterParams['city_id'] != '' 
+            @properties = @properties.where("city_id = ?", @filterParams['city_id'])
+          end
+          
+          if @filterParams['area_code_id'] != nil and @filterParams['area_code_id'] != '' 
+            @properties = @properties.where("area_code_id = ?", @filterParams['area_code_id'])
+          end
+          
+          if @filterParams['property_broker_id'] != nil and @filterParams['property_broker_id'] != ''
+            @properties = @properties.joins(:brokers)
+            @properties = @properties.where("broker_id = ?", @filterParams['property_broker_id'])
+          end
+          
+        end
+        
+        
       end
       
 
